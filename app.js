ae2e78b9a4ff2afee43cc8245385bbc831b2373a
DESKTOP-VBC15AG\Administrator <catcolor2020@gmail.com>
2022-01-19 17:05:00 +0800

    project init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const app = express()
+const PORT = 3000
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
