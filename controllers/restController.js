e906cff7ceddb335c4436915f55c44c99b118ed4
DESKTOP-VBC15AG\Administrator <catcolor2020@gmail.com>
2022-01-24 01:29:08 +0800

    add search keyword view

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,10 +1,13 @@
 const { Restaurant, Category } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 const restController = {
   getRestaurants: (req, res, next) => {
     const DEFAULT_LIMIT = 9
     const categoryId = Number(req.query.categoryId) || ''
+    const keyword = req.query.keyword ? req.query.keyword : ''
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || DEFAULT_LIMIT
     const offset = getOffset(limit, page)
@@ -13,13 +16,19 @@ const restController = {
         include: Category,
         where: {
           ...categoryId ? { categoryId } : {}
+          // [Op.or]: [
+          //   { ...categoryId ? { categoryId } : {} },
+          //   { name: new RegExp(keyword, 'i') },
+          // ]
         },
         limit,
         offset,
         nest: true,
         raw: true
       }),
-      Category.findAll({ raw: true })
+      Category.findAll({
+        raw: true
+      })
     ])
       .then(([restaurants, categories]) => {
         const data = restaurants.rows.map(r => ({
@@ -30,6 +39,7 @@ const restController = {
           restaurants: data,
           categories,
           categoryId,
+          keyword,
           pagination: getPagination(limit, page, restaurants.count)
         })
       })
