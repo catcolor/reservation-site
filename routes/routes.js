fc1e5ec0a82b692f84836636855adf1fd19123c8
DESKTOP-VBC15AG\Administrator <catcolor2020@gmail.com>
2022-01-24 01:54:52 +0800

    user singup

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restController.js')
+const userController = require('../controllers/userController.js')
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 router.get('/', (req, res) => {
   res.redirect('/restaurants')
