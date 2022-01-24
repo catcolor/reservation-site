d109cfd31caaeb1e5e86a69de8d68abd4342f7c9
DESKTOP-VBC15AG\Administrator <catcolor2020@gmail.com>
2022-01-25 04:43:09 +0800

    create restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,6 +8,25 @@ const adminController = {
         res.render('admin/restaurants', { restaurants })
       )
       .catch(next)
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('餐廳名稱必填 ! ')
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', '餐廳成功編輯!')
+        res.redirect('admin/restaurants')
+      })
+      .catch(next)
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const adminController = require('../../controllers/adminController')
 
+router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
